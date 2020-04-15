757b3f41d5fb67824ab4f5a50c31fd2d58f9a66a
Volodymyr Tkachuk <tkachuk.v4@gmail.com>
2020-04-15 14:36:00 -0400

    fixed test name typo

--- tests/unit_tests/test_problem_5_p_18.py
+++ tests/unit_tests/test_problem_5_p_18.py
@@ -13,5 +13,5 @@ class TestProblem5P18(object):
         ([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]],
          [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]),
     ])
-    def test_random_sampling(self, two_d_list, spiral_ordered_list):
+    def test_spiral_ordering(self, two_d_list, spiral_ordered_list):
         assert self.instantiate_solution().spiral_ordering(two_d_list) == spiral_ordered_list
