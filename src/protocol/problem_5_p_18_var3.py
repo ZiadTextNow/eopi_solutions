9e4523019e7858c1a082ce288903baf0dacf0584
Volodymyr Tkachuk <tkachuk.v4@gmail.com>
2020-04-15 14:25:38 -0400

    Added protocol for problem 5.18var3 but didn't understand what it meant so didn't solve it

---
+++ src/protocol/problem_5_p_18_var3.py
@@ -0,0 +1,8 @@
+from protocol.errors import EOPINotImplementedError
+
+# TODO: I am very confused what this problem means!!! Please message me once you get here!
+
+class Problem5P18Var3(object):
+    def enumerate_int_pairs(self, num_pairs):
+        raise EOPINotImplementedError()
+
