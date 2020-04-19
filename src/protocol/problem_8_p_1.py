2442e93340209149cb47c86f789b12bc609d2565
Volodymyr Tkachuk <tkachuk.v4@gmail.com>
2020-04-19 11:35:40 -0400

    fixed syntax error in raising errors

--- src/protocol/problem_8_p_1.py
+++ src/protocol/problem_8_p_1.py
@@ -4,10 +4,10 @@ from protocol.errors import EOPINotImplementedError
 class Problem8P1(object):
     class StackWithMax(object):
         def max(self):
-            EOPINotImplementedError()
+            raise EOPINotImplementedError()
 
         def push(self, elem):
-            EOPINotImplementedError()
+            raise EOPINotImplementedError()
 
         def pop(self):
-            EOPINotImplementedError()
+            raise EOPINotImplementedError()
