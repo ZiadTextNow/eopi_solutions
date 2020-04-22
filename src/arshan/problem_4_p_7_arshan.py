66dbb2494c37db565aa9a3864d0e26d40ce58803
Arshankhanifar <arshankhanifar@gmail.com>
2020-04-22 09:31:34 -0400

    changed the tests a little bit, will need to talk to vlad about it

--- src/arshan/problem_4_p_7_arshan.py
+++ src/arshan/problem_4_p_7_arshan.py
@@ -3,16 +3,30 @@ from protocol.problem_4_p_7 import Problem4P7
 
 class Problem4P7Arshan(Problem4P7):
     def x_to_the_y(self, x, y):
+        return self._power_using_lookup(x, y)
         return self._faster_divide_and_conquer(x, y)
         return self._brute_force(x, y)
         return self._lazy_solution(x, y)
 
+    """
+    this is O(log(n))
+    """
+    def _power_using_lookup(self, x, y):
+        powers = [1, x]
+        i = 1
+        while (1 << i) < y:
+            powers.append(powers[i] * powers[i])
+            i += 1
+        result = 1
+        j = 1
+        while y:
+            if y & 1:
+                result *= (powers[j] * (y & 1))
+            j += 1
+            y >>= 1
+        return result
 
-    """
-    probably not faster
-    """
     def _faster_divide_and_conquer(self, x, y):
-        print(x, y)
         if y == 0:
             return 1
         if y == 1:
