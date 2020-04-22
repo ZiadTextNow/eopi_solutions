f825a13bca6d31cb26d66e5be4b3c5dec843d673
Arshankhanifar <arshankhanifar@gmail.com>
2020-04-22 11:03:12 -0400

    added another solution to 5.1, tried out the books solution to 5.1, fixed the failing test for in-place solutions (both vlads solutions and my solution pass now)

--- src/arshan/problem_5_p_1_arshan.py
+++ src/arshan/problem_5_p_1_arshan.py
@@ -3,6 +3,56 @@ from protocol.problem_5_p_1 import Problem5P1
 
 class Problem5P1Arshan(Problem5P1):
     def dutch_flag_partition(self, pivot_index, unpartitioned_list):
+        return self._less_space(pivot_index, unpartitioned_list)
+        return self._books_solution(pivot_index, unpartitioned_list)
+        return self._lazy_solution(pivot_index, unpartitioned_list)
+
+    def _books_solution(self, pivot_index, A):
+        pivot = A[pivot_index]
+        for i in range(len(A)):
+            for j in range(i+1, len(A)):
+                if A[j] < pivot:
+                    A[i], A[j] = A[j], A[i]
+                    break
+        for i in reversed(range(len(A))):
+            if A[i] < pivot:
+                break
+            for j in reversed(range(i)):
+                if A[j] > pivot:
+                    A[i], A[j] = A[j], A[i]
+                    break
+        return A
+    """
+    O(n^2): worst complexity
+    idea: 
+         - do a lil swappy swap if it's to the left of pivot
+         - do a lil shifty shift if it's to the right of pivot
+    best case:
+        - more swaps will result in less complexity O(n)
+        - more shifts will probably result in more complexity
+    """
+    def _less_space(self, pivot_index, unpartitioned_list):
+        i = 0
+        while i < len(unpartitioned_list):
+            if i < pivot_index:
+                if unpartitioned_list[i] > unpartitioned_list[pivot_index]:
+                    unpartitioned_list[i], unpartitioned_list[pivot_index] = unpartitioned_list[pivot_index], unpartitioned_list[i]
+                    pivot_index = i
+            else:
+                if unpartitioned_list[i] < unpartitioned_list[pivot_index]:
+                    unpartitioned_list[pivot_index:i+1] = [unpartitioned_list[i]] + unpartitioned_list[pivot_index:i]
+                    pivot_index += 1
+            i += 1
+        return unpartitioned_list
+
+    """
+    O(n)
+    Go through the array 3 times:
+    append all less
+    append all equal
+    append all bigger
+    """
+    def _lazy_solution(self, pivot_index, unpartitioned_list):
         partitioned = []
         pivot_value = unpartitioned_list[pivot_index]
         for i, value in enumerate(unpartitioned_list):
@@ -15,9 +65,6 @@ class Problem5P1Arshan(Problem5P1):
             if value > pivot_value:
                 partitioned.append(value)
         return partitioned
-
-
-
 
 
 
--- tests/unit_tests/test_problem_5_p_1.py
+++ tests/unit_tests/test_problem_5_p_1.py
@@ -6,22 +6,20 @@ from vlad.problem_5_p_1_vlad import Problem5P1Vlad
 
 class TestProblem5P1(object):
     def instantiate_solution(self):
-        #return Problem5P1Arshan()
-        return Problem5P1Vlad()
+        return Problem5P1Arshan()
+        #return Problem5P1Vlad()
 
-    """
-    equal_idx is leftmost idx where value == pivot
-    high_idx is leftmost idx where value > pivot. If no values in list exist that are > pivot then high_idx is None 
-    """
     @pytest.mark.parametrize("pivot_index, unpartitioned_list", [
         (3, [-3, 0, -1, 1, 1, -5, 1, 3, 4, 2]),
         (0, [0]),
-        (2, [9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+        (2, [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
+        (5, [10, 9, 8, 7, 6, 5])
     ])
     def test_dutch_flag_partition(self, pivot_index, unpartitioned_list):
-        partitioned_list = self.instantiate_solution().dutch_flag_partition(pivot_index, unpartitioned_list)
         pivot_value = unpartitioned_list[pivot_index]
         first_index_beq_pivot = len(unpartitioned_list)
+        partitioned_list = self.instantiate_solution().dutch_flag_partition(pivot_index, unpartitioned_list)
+        assert len(partitioned_list) == len(unpartitioned_list)
         for i, value in enumerate(partitioned_list):
             if value >= pivot_value:
                 first_index_beq_pivot = i
